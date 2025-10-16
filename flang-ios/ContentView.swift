e470aeaddccee056667d71f3d1e9e505f79d790d
Tim Fraedrich <timfraedrich@icloud.com>
2025-10-16 10:18:13 +0200

    initial commit

---
+++ flang-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flang-ios
+//
+//  Created by Tim Fraedrich on 16.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flang-ios/flang_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flang_iosApp.swift
+//  flang-ios
+//
+//  Created by Tim Fraedrich on 16.10.25.
+//
+
+import SwiftUI
+
+@main
+struct flang_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
